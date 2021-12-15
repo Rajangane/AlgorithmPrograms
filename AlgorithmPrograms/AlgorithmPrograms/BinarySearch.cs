051b5b7d38f8f784e8761a810f0784be929a866f
Rajangane <pratiksharajangane7@gmail.com>
2021-12-15 19:25:42 +0530

    [Pratiksha] ADD : Binary Search For a word

---
+++ AlgorithmPrograms/AlgorithmPrograms/BinarySearch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    public class BinarySearch
+    {
+        public void binarysearch()
+        {
+            string sampleademo = "your mind belives what you keep telling it again and again so tell positive things only";
+            string[] Demo = sampleademo.Split(" "); //Seprating a string
+
+            Array.Sort(Demo); // sorting Array in Assending Order
+
+            Console.WriteLine("Enter word you want to search : ");
+            string search = Console.ReadLine();
+           //this method search value in a one dimensional array
+            int i = Array.BinarySearch(Demo, search);//taking index value
+
+            if (i >= 0)
+                Console.WriteLine("word found at position " + i + "\n");
+            else
+                Console.WriteLine("word not found\n");
+        }
+    }
+}
--- AlgorithmPrograms/AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/AlgorithmPrograms/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("**************** Welcom To Data Structure & Algorithms ******************");
+//Console.WriteLine("**************** Welcom To Data Structure & Algorithms ******************");
+using AlgorithmPrograms;
+Console.Write("Select Number:\n1)BinarySearch");
+int option = Convert.ToInt32(Console.ReadLine());
+
+switch (option)
+{
+    case 1:
+        BinarySearch bs = new BinarySearch();
+        bs.binarysearch();
+        break;
+
+    default:
+        Console.WriteLine("Invalid Option");
+        break;
+}
