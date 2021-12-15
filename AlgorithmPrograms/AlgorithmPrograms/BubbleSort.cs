de252bf435ebd27e515edd89391e24b8823d9fdf
Rajangane <pratiksharajangane7@gmail.com>
2021-12-15 20:22:32 +0530

    [Pratiksha] ADD : Bubble Sort For a integer number

---
+++ AlgorithmPrograms/AlgorithmPrograms/BubbleSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    public class BubbleSort
+    {
+        public void Bubblesort()
+        {
+            int[] arr = { 100, 55, 200, 16 };
+            int temp;
+            for (int j = 0; j <= arr.Length - 2; j++)
+            {
+                for (int i = 0; i <= arr.Length - 2; i++)
+                {
+                    if (arr[i] > arr[i + 1]) //if num > num2 then swap
+                    {
+                        temp = arr[i + 1];// swap temp and arr[i]
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                        Console.WriteLine("Sorted array:");
+                        foreach (int p in arr) //print array
+                            Console.WriteLine(p + " ");
+                    }
+                }
+            }
+        }
+    }
+}
--- AlgorithmPrograms/AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/AlgorithmPrograms/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("**************** Welcom To Data Structure & Algorithms ******************");
 using AlgorithmPrograms;
-Console.Write("Select Number:\n1)BinarySearch\n2)InsertionSort");
+Console.Write("Select Number:\n1)BinarySearch\n2)InsertionSort\n3)BubbleSort");
 int option = Convert.ToInt32(Console.ReadLine());
 
 switch (option)
@@ -13,7 +13,12 @@ switch (option)
     case 2:
         InsertionSort insertionSort = new InsertionSort();
         insertionSort.Insert();
-       
+        break;
+    case 3:
+        BubbleSort bubbleSort = new BubbleSort();
+        bubbleSort.Bubblesort();
+        break;
+
 
         break;
 
